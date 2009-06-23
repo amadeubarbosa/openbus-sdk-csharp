c1a7dd3c9b6336e6fcc651c28a7d1ec3c0061e5b
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2009-06-23 21:07:48 +0000

    [OPENBUS-251] Criacao de teste para o Utils.lua.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core@92751 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ test/lua/testUtils.lua
@@ -0,0 +1,37 @@
+--
+-- Testes unitários do arquivo Utils.lua
+--
+-- $Id: testUtils.lua $
+--
+local oil = require "oil"
+local orb = oil.orb
+
+local CredentialManager = require "openbus.common.CredentialManager"
+local ClientInterceptor = require "openbus.common.ClientInterceptor"
+local Utils = require "openbus.util.Utils"
+
+Suite = {
+  Test1 = {
+    beforeTestCase = function(self)
+      local IDLPATH_DIR = os.getenv("IDLPATH_DIR")
+      if IDLPATH_DIR == nil then
+        io.stderr:write("A variavel IDLPATH_DIR nao foi definida.\n")
+        os.exit(1)
+      end
+      local idlfile = IDLPATH_DIR.."/access_control_service.idl"
+      orb:loadidlfile(idlfile)
+     end,
+    testFetchACS = function(self)
+      local acs, lp, ic = Utils.fetchAccessControlService(orb, "localhost", 2089)
+      local user = "tester"
+      local password = "tester"
+      local DATA_DIR = os.getenv("OPENBUS_DATADIR")
+      local config = assert(loadfile(DATA_DIR.."/conf/advanced/InterceptorsConfiguration.lua"))()
+      local credentialManager = CredentialManager()
+      orb:setclientinterceptor(ClientInterceptor(config, credentialManager))
+      local success, credential = acs:loginByPassword(user, password)
+      credentialManager:setValue(credential)
+      acs:logout(credential)
+    end,
+ }
+}
