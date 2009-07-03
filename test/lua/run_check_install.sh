5a6db23842c357a5d19e05aefa7566ec6b7c518e
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2009-07-03 13:50:40 +0000

    Script mais userfriendly. A senha não aparece mais na tela.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core@93260 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/lua/run_check_install.sh
+++ test/lua/run_check_install.sh
@@ -1,5 +1,22 @@
 #!/bin/ksh
+echo
+echo --- Testes do Openbus ---
 
-PARAMS=$*
+echo -n "host:"
+read HOST
 
-${OPENBUS_HOME}/core/bin/servicelauncher checkInstall.lua  ${PARAMS}
+echo -n "port:"
+read PORT
+
+echo -n "usuario:"
+read USER
+
+echo -n "senha:"
+stty -echo
+read PASSWORD
+stty echo 
+echo
+
+echo
+echo Iniciando testes.
+${OPENBUS_HOME}/core/bin/servicelauncher checkInstall.lua  ${HOST} ${PORT} ${USER} ${PASSWORD}
