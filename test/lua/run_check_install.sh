2157db0d5b3f661ff70186146f91ee75a551fe63
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2009-10-16 19:19:51 +0000

    Varificar se as variaveis de ambiente OPENBUS_HOME e TEC_UNAME estao configuradas. Esta verificacao foi necessaria visto que o teste esta sendo enviando no pacote 'fulladmin' (Issue [OPENBUS-414]) e administradores de barramentos podem nao possuir estas variaveis.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core@97640 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/lua/run_check_install.sh
+++ test/lua/run_check_install.sh
@@ -1,6 +1,16 @@
 #!/bin/ksh
 TMPFILE=`mktemp`
 
+if [ -z "$TEC_UNAME" ]; then
+    echo "a variável TEC_UNAME não está definida."
+    exit 1
+fi
+
+if [ -z "$OPENBUS_HOME" ]; then
+    echo "a variável OPENBUS_HOME não está definida."
+    exit 1
+fi
+
 echo
 echo --- Testes do Openbus ---
 
