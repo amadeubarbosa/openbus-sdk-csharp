9489030f1fc77a6e6cc630f111953359675ab037
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2011-08-11 20:40:13 +0000

    [OPENBUS-1504] Padronizar os nomes dos artefatos gerados
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/trunk@120959 ae0415b3-e90b-0410-900d-d0be9363c56b

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenbusAPI")]
+[assembly: AssemblyTitle("Openbus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PUC-Rio")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.5.4.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
