8df800801d0767ab249f270719f92e98ac7b93ec
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2011-09-02 17:27:14 +0000

    [OPENBUS-1507] Remover dependência com NUnit  - Adicionando o MSTest
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/trunk@121656 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ Test/OpenbusTest.cs
@@ -0,0 +1,397 @@
+﻿using Tecgraf.Openbus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tecgraf.Openbus.Exception;
+using System.Configuration;
+using Tecgraf.Openbus.Lease;
+using tecgraf.openbus.core.v1_05.registry_service;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Tecgraf.Openbus.Security;
+using tecgraf.openbus.core.v1_05.access_control_service;
+
+namespace Test
+{
+  /// <summary>
+  /// Classe responsável por testar o método de inicializar e finalizar o Openbus.
+  /// </summary>
+  [TestClass]
+  public class OpenbusTest
+  {
+    #region Fields
+
+    private TestContext testContextInstance;
+    public TestContext TestContext {
+      get {
+        return testContextInstance;
+      }
+      set {
+        testContextInstance = value;
+      }
+    }
+
+    private static String userLogin;
+    private static String userPassword;
+    private static String hostName;
+    private static int hostPort;
+
+    private static String testKeyFileName;
+    private static String acsCertificateFileName;
+    private static String entityName;
+
+    #endregion
+
+    #region Preparation
+
+    [ClassInitialize]
+    public static void BeforeClass(TestContext testContext) {
+      hostName = System.Configuration.ConfigurationSettings.AppSettings["hostName"];
+      if (String.IsNullOrEmpty(hostName))
+        throw new ArgumentNullException("hostName");
+
+      string port = System.Configuration.ConfigurationSettings.AppSettings["hostPort"];
+      hostPort = Int32.Parse(port);
+
+      userLogin = ConfigurationSettings.AppSettings["userLogin"];
+      if (String.IsNullOrEmpty(userLogin))
+        throw new ArgumentNullException("userLogin");
+
+      userPassword = ConfigurationSettings.AppSettings["userPassword"];
+      if (String.IsNullOrEmpty(userPassword))
+        throw new ArgumentNullException("userPassword");
+
+      testKeyFileName = ConfigurationSettings.AppSettings["testKeyFileName"];
+      if (String.IsNullOrEmpty(testKeyFileName))
+        throw new ArgumentNullException("testKeyFileName");
+
+      acsCertificateFileName = ConfigurationSettings.AppSettings["acsCertificateFileName"];
+      if (String.IsNullOrEmpty(acsCertificateFileName))
+        throw new ArgumentNullException("acsCertificateFileName");
+
+      entityName = ConfigurationSettings.AppSettings["entityName"];
+      if (String.IsNullOrEmpty(entityName))
+        throw new ArgumentNullException("entityName");
+    }
+
+    /// <summary>
+    /// Este método é chamado antes de cada testCase.
+    /// </summary>
+    [TestInitialize]
+    public void BeforeEachTest() {
+      Openbus openbus = Openbus.GetInstance();
+      openbus.Init(hostName, hostPort);
+    }
+
+    /// <summary>
+    /// Este método é chamado depois de cada testCase.
+    /// </summary>
+    [TestCleanup]
+    public void AfterEachTest() {
+      Openbus openbus = Openbus.GetInstance();
+      openbus.Destroy();
+    }
+
+    #endregion
+
+    #region Tests
+
+    #region API
+
+    /// <summary>
+    /// Testa o connect passando usuário e senha válido.
+    /// </summary>
+    [TestMethod]
+    public void ConnectByPassword() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = openbus.Connect(userLogin, userPassword);
+      Assert.IsNotNull(registryService);
+      Assert.IsTrue(openbus.Disconnect());
+    }
+
+    /// <summary>
+    /// Testa o connect passando login null.
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ConnectByPassword_NullLogin() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = null;
+      registryService = openbus.Connect(null, userPassword);
+    }
+
+    /// <summary>
+    /// Testa o connect passando password null.
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ConnectByPassword_NullPassword() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = null;
+      registryService = openbus.Connect(userLogin, null);
+    }
+
+    /// <summary>
+    /// Testa o connect passando usuário inválido.
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ACSLoginFailureException))]
+    public void ConnectByPassword_InvalidLogin() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = null;
+      registryService = openbus.Connect("null", "null");
+    }
+
+    /// <summary>
+    /// Testa conectar duas vezes ao barramento utilizando o método connect 
+    /// por usuário e senha.
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ACSLoginFailureException))]
+    public void ConnectByPassword_Twice() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = openbus.Connect(userLogin, userPassword);
+      Assert.IsNotNull(registryService);
+      registryService = null;
+      openbus.Connect(userLogin, userPassword);
+    }
+
+    /// <summary>
+    /// Testa o connect utilizando o certificado.
+    /// </summary>
+    [TestMethod]
+    public void ConnectByCertificate() {
+      Openbus openbus = Openbus.GetInstance();
+      RSACryptoServiceProvider privateKey = Crypto.ReadPrivateKey(testKeyFileName);
+      Assert.IsNotNull(privateKey);
+
+      X509Certificate2 acsCertificate =
+        Crypto.ReadCertificate(acsCertificateFileName);
+      Assert.IsNotNull(acsCertificate);
+
+      IRegistryService registryService =
+        openbus.Connect(entityName, privateKey, acsCertificate);
+
+
+      Assert.IsNotNull(registryService);
+
+
+      Assert.IsTrue(openbus.Disconnect());
+    }
+
+    /// <summary>
+    /// Testa o connect por certificado passando o entity name inválido
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ACSLoginFailureException))]
+    public void ConnectByCertificate_InvalidEntityName() {
+      Openbus openbus = Openbus.GetInstance();
+      RSACryptoServiceProvider privateKey = Crypto.ReadPrivateKey(testKeyFileName);
+      Assert.IsNotNull(privateKey);
+
+      X509Certificate2 acsCertificate =
+        Crypto.ReadCertificate(acsCertificateFileName);
+      Assert.IsNotNull(acsCertificate);
+      IRegistryService registryService = null;
+      registryService = openbus.Connect("null", privateKey, acsCertificate);
+    }
+
+    /// <summary>
+    /// Testa o connect por certificado passando a cheve privada nula. 
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ConnectByCertificate_NullKey() {
+      Openbus openbus = Openbus.GetInstance();
+
+      X509Certificate2 acsCertificate =
+        Crypto.ReadCertificate(acsCertificateFileName);
+      Assert.IsNotNull(acsCertificate);
+      IRegistryService registryService = null;
+      registryService = openbus.Connect(entityName, null, acsCertificate);
+    }
+
+    /// <summary>
+    /// Testa o connect por certificado passando o certificado digital nulo. 
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ConnectByCertificate_NullACSCertificate() {
+      Openbus openbus = Openbus.GetInstance();
+      RSACryptoServiceProvider privateKey = Crypto.ReadPrivateKey(testKeyFileName);
+      Assert.IsNotNull(privateKey);
+
+      IRegistryService registryService = null;
+      registryService = openbus.Connect(entityName, privateKey, null);
+    }
+
+    /// <summary>
+    /// Testa conectar duas vezes ao barramento utilizando o método connect 
+    /// por certificado
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ACSLoginFailureException))]
+    public void ConnectByCertificate_Twice() {
+      Openbus openbus = Openbus.GetInstance();
+      RSACryptoServiceProvider privateKey = Crypto.ReadPrivateKey(testKeyFileName);
+      Assert.IsNotNull(privateKey);
+
+      X509Certificate2 acsCertificate =
+        Crypto.ReadCertificate(acsCertificateFileName);
+      Assert.IsNotNull(acsCertificate);
+
+      IRegistryService registryService =
+        openbus.Connect(entityName, privateKey, acsCertificate);
+      Assert.IsNotNull(registryService);
+
+      registryService = null;
+      registryService = openbus.Connect(entityName, privateKey, acsCertificate);
+    }
+
+    /// <summary>
+    /// Testa o connect utilizando a credencial.
+    /// </summary>
+    [TestMethod]
+    public void ConnectByCredential() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = openbus.Connect(userLogin, userPassword);
+      Assert.IsNotNull(registryService);
+
+      Credential myCredential = openbus.Credential;
+      registryService = openbus.Connect(myCredential);
+      Assert.IsNotNull(registryService);
+      Assert.IsTrue(openbus.Disconnect());
+    }
+
+    /// <summary>
+    /// Testa o connect por credencial passando a credencial nula.
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ConnectByCredential_NullCredencial() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = openbus.Connect(userLogin, userPassword);
+      Assert.IsNotNull(registryService);
+      registryService = null;
+
+      Credential myCredencial = new Credential();
+      registryService = openbus.Connect(myCredencial);
+    }
+
+    /// <summary>
+    /// Testa o connect por credencial passando a credencial inválida.
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(InvalidCredentialException))]
+    public void ConnectByCredential_InvalidCredencial() {
+      Openbus openbus = Openbus.GetInstance();
+      IRegistryService registryService = openbus.Connect(userLogin, userPassword);
+      Assert.IsNotNull(registryService);
+      registryService = null;
+
+      Credential myCredential = new Credential("null", "null", "");
+      registryService = openbus.Connect(myCredential);
+    }
+
+    /// <summary>
+    /// Verifica se uma credencial nula é retornada como credencial interceptada
+    /// quando não estamos na thread de uma chamada remota.
+    /// </summary>
+    [TestMethod]
+    public void GetInterceptedCredential() {
+      Openbus openbus = Openbus.GetInstance();
+      Assert.AreEqual(openbus.GetInterceptedCredential(), new Credential());
+    }
+
+    /// <summary>
+    /// Testa o IsConnected
+    /// </summary>
+    [TestMethod]
+    public void IsConnected() {
+      Openbus openbus = Openbus.GetInstance();
+      Assert.IsFalse(openbus.IsConnected());
+      Assert.IsNotNull(openbus.Connect(userLogin, userPassword));
+      Assert.IsTrue(openbus.IsConnected());
+      Assert.IsTrue(openbus.Disconnect());
+      Assert.IsFalse(openbus.IsConnected());
+    }
+
+    /// <summary>
+    /// Testa se disconectar do barramento.
+    /// </summary>
+    [TestMethod]
+    public void Disconnect() {
+      Openbus openbus = Openbus.GetInstance();
+      Assert.IsFalse(openbus.Disconnect());
+      Assert.IsNotNull(openbus.Connect(userLogin, userPassword));
+      Assert.IsTrue(openbus.Disconnect());
+      Assert.IsFalse(openbus.Disconnect());
+    }
+
+    /// <summary>
+    /// Testa o GetAccessControlService
+    /// </summary>
+    [TestMethod]
+    public void GetAccessControlService() {
+      Openbus openbus = Openbus.GetInstance();
+      Assert.IsNull(openbus.GetAccessControlService());
+      Assert.IsNotNull(openbus.Connect(userLogin, userPassword));
+      Assert.IsNotNull(openbus.GetAccessControlService());
+      Assert.IsTrue(openbus.Disconnect());
+      Assert.IsNull(openbus.GetAccessControlService());
+    }
+
+    /// <summary>
+    /// Testa o GetRegistryService
+    /// </summary>
+    [TestMethod]
+    public void GetRegistryService() {
+      Openbus openbus = Openbus.GetInstance();
+      Assert.IsNull(openbus.GetRegistryService());
+      Assert.IsNotNull(openbus.Connect(userLogin, userPassword));
+      Assert.IsNotNull(openbus.GetRegistryService());
+      Assert.IsTrue(openbus.Disconnect());
+      Assert.IsNull(openbus.GetRegistryService());
+    }
+    
+    #endregion
+
+    #region Private Classes
+
+    /// <summary>
+    /// Classe criada para testar o Lease Expired Callback. Utilizada nos
+    /// testes unitários <i>addLeaseExpiredCbAfterConnect</i> e 
+    /// <i>addLeaseExpiredCbBeforeConnect</i>
+    /// </summary>
+    private class LeaseExpiredCbReconnect : LeaseExpiredCallback
+    {
+      private bool reconnected;
+      private String userLogin;
+      private String userPassword;
+
+      public LeaseExpiredCbReconnect(String userLogin, String userPassword) {
+        this.reconnected = false;
+        this.userLogin = userLogin;
+        this.userPassword = userPassword;
+      }
+
+      public bool isReconnected() {
+        return this.reconnected;
+      }
+
+      void LeaseExpiredCallback.Expired() {
+        Openbus openbus = Openbus.GetInstance();
+        try {
+          openbus.Connect(userLogin, userPassword);
+          this.reconnected = true;
+        }
+        catch (OpenbusException) {
+          this.reconnected = false;
+        }
+      }
+    }
+
+    #endregion
+
+    #endregion
+  }
+}
--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Openbus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("Teste unitario utilizando MSTest")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("PUC-Rio")]
-[assembly: AssemblyProduct("Openbus API")]
-[assembly: AssemblyCopyright("Copyright © PUC-Rio 2009")]
+[assembly: AssemblyCompany("Tecgraf")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © Tecgraf 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f95d090-d29b-4536-9c2a-da30b6241679")]
+[assembly: Guid("8490d70a-3143-4c6c-812b-e45a093e5001")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.4.0")]
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fbdca0c2e21da54964d502e77313458ed93ea633f9111105f1b72b41220cf0d4d83ef086ecd988bc3d7149b75481de78c659c49256e6d4011021cc0d51f81e9cd96760344f509ae6da367ede0fc5d7554bfab8e6ccb7498c37f1ca906f082b1bb0d4e9915eb12a8baef54a6c09a15a1ab1d534bee963b64b73b4e3c7cc5b5fc8")]
