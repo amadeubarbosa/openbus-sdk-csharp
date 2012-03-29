5694ff80168cf4ebcc59c8204e54a52a68fd2514
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-03-29 06:24:33 +0000

    [OPENBUS-1541] Implementar a nova proposta do SDK OpenBus em C# - COMMIT PARCIAL - Finalização da implementação inicial da demo delegation. Falta testar.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@127663 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/delegate/Client/src/DelegationClient.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Ch.Elca.Iiop.Idl;
+using Client.Properties;
+using log4net.Config;
+using tecgraf.openbus.core.v2_00.services.offer_registry;
+using tecgraf.openbus.demo.delegation;
+using tecgraf.openbus.sdk;
+using tecgraf.openbus.sdk.Standard;
+
+namespace Client {
+  internal static class DelegationClient {
+    private static Connection _conn;
+    private static Messenger _messenger;
+    private static Broadcaster _broadcaster;
+    private static Forwarder _forwarder;
+
+    private static void Main(string[] args) {
+      AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+      string hostName = DemoConfig.Default.hostName;
+      int hostPort = DemoConfig.Default.hostPort;
+
+      FileInfo logFileInfo = new FileInfo(DemoConfig.Default.logFile);
+      XmlConfigurator.ConfigureAndWatch(logFileInfo);
+
+      OpenBus openbus = StandardOpenBus.Instance;
+      _conn = openbus.Connect(hostName, (short) hostPort);
+
+      string userLogin = DemoConfig.Default.userLogin;
+      string userPassword = DemoConfig.Default.userPassword;
+      System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+      _conn.LoginByPassword(userLogin, encoding.GetBytes(userPassword));
+
+      GetServices();
+
+      _conn.Logout();
+
+      _conn.LoginByPassword("willian", encoding.GetBytes("willian"));
+      _forwarder.setForward("bill");
+      _broadcaster.subscribe();
+      _conn.Logout();
+
+      _conn.LoginByPassword("paul", encoding.GetBytes("paul"));
+      _broadcaster.subscribe();
+      _conn.Logout();
+
+      _conn.LoginByPassword("mary", encoding.GetBytes("mary"));
+      _broadcaster.subscribe();
+      _conn.Logout();
+
+      _conn.LoginByPassword("steve", encoding.GetBytes("steve"));
+      _broadcaster.subscribe();
+      _broadcaster.post("Testando a lista!");
+      _conn.Logout();
+
+      Console.WriteLine("Esperando as mensagens propagarem.");
+      Thread.Sleep(10000);
+      Console.WriteLine("Pronto!");
+
+      string[] names = new[]{"willian", "bill", "paul", "mary", "steve"};
+      foreach (string name in names) {
+        _conn.LoginByPassword(name, encoding.GetBytes(name));
+        ShowPostsOf(name, _messenger.receivePosts());
+        _broadcaster.unsubscribe();
+        _conn.Logout();
+      }
+      
+      _conn.LoginByPassword("willian", encoding.GetBytes("willian"));
+      _forwarder.cancelForward("bill");
+      _conn.Logout();
+    }
+
+    private static void ShowPostsOf(string user, PostDesc[] posts) {
+      Console.WriteLine(user + " recebeu " + posts.Length + " mensagens:");
+      for (int i = 0; i < posts.Length; i++) {
+        Console.WriteLine(i + ") " + posts[i].from + ": " + posts[i].message);
+      }
+      Console.WriteLine();
+    }
+
+    private static void GetServices() {
+      // propriedade definida pelos servidores
+      ServiceProperty prop = new ServiceProperty("offer.domain", "OpenBus Demos");
+
+      ServiceProperty[] properties = new[] {prop};
+      ServiceOfferDesc[] offers = _conn.OfferRegistry.findServices(properties);
+
+      if (offers.Length != 3) {
+        Console.WriteLine("Há mais serviços do que o esperado no barramento.");
+        Console.Read();
+        Environment.Exit(1);
+      }
+
+      foreach (ServiceOfferDesc serviceOfferDesc in offers) {
+        string repId = String.Empty;
+        ServiceProperty[] props = serviceOfferDesc.properties;
+        foreach (ServiceProperty serviceProperty in props) {
+          if (serviceProperty.name.Equals("openbus.component.interface")) {
+            repId = serviceProperty.value;
+          }
+        }
+        MarshalByRefObject obj = serviceOfferDesc.service_ref.getFacet(repId);
+        if (obj == null) {
+          Console.WriteLine("Não foi possível encontrar a faceta do tipo " +
+                            repId);
+          return;
+        }
+        Type type = Repository.GetTypeForId(repId);
+        if (type == typeof (Messenger)) {
+          _messenger = obj as Messenger;
+          continue;
+        }
+        if (type == typeof (Broadcaster)) {
+          _broadcaster = obj as Broadcaster;
+          continue;
+        }
+        if (type == typeof (Forwarder)) {
+          _forwarder = obj as Forwarder;
+        }
+      }
+    }
+
+    private static void CurrentDomain_ProcessExit(object sender, EventArgs e) {
+      _conn.Close();
+    }
+  }
+}
