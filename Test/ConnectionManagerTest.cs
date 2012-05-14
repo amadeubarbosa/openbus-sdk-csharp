b4bb6b431ac7d06a1f9ee317b00f951eeef7e254
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-05-14 23:34:09 +0000

    [OPENBUS-1706] Implementar testes para o SDK CSharp 2.0 - Inclusão dos testes para a classe ORBInit. - Inclusão dos testes para a interface ConnectionManager. - Os testes da interface Connection ainda não estão implementados.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@129024 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ Test/ConnectionManagerTest.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Configuration;
+using tecgraf.openbus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test {
+  /// <summary>
+  ///This is a test class for ConnectionManagerTest and is intended
+  ///to contain all ConnectionManagerTest Unit Tests
+  ///</summary>
+  [TestClass]
+  public class ConnectionManagerTest {
+    #region Fields
+
+    private static String _hostName;
+    private static short _hostPort;
+    private static String _entity;
+    private static ConnectionManager _manager;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext { get; set; }
+
+    #endregion
+
+    #region Additional test attributes
+
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+
+    #endregion
+
+    //Use ClassInitialize to run code before running the first test in the class
+    [ClassInitialize]
+    public static void MyClassInitialize(TestContext testContext) {
+      _hostName = ConfigurationManager.AppSettings["hostName"];
+      if (String.IsNullOrEmpty(_hostName)) {
+        throw new ArgumentNullException("hostName");
+      }
+
+      string port = ConfigurationManager.AppSettings["hostPort"];
+      _hostPort = short.Parse(port);
+
+      _entity = ConfigurationManager.AppSettings["entityName"];
+      if (String.IsNullOrEmpty(_entity)) {
+        throw new ArgumentNullException("entityName");
+      }
+
+      _manager = ORBInitializer.Manager;
+    }
+
+    /// <summary>
+    ///A test for ORB
+    ///</summary>
+    [TestMethod]
+    public void ORBTest() {
+      Assert.IsNotNull(ORBInitializer.Manager.ORB);
+    }
+
+    /// <summary>
+    ///A test for CreateConnection
+    ///</summary>
+    [TestMethod]
+    public void CreateConnectionTest() {
+      // cria conexão válida
+      Connection valid = _manager.CreateConnection(_hostName, _hostPort);
+      Assert.IsNotNull(valid);
+      // tenta criar conexão com hosts inválidos
+      Connection invalid = null;
+      try {
+        invalid = _manager.CreateConnection("", _hostPort);
+      }
+      catch (Exception) {
+      }
+      finally {
+        Assert.IsNull(invalid);
+      }
+      try {
+        invalid = _manager.CreateConnection(_hostName, -1);
+      }
+      catch (Exception) {
+      }
+      finally {
+        Assert.IsNull(invalid);
+      }
+    }
+
+    /// <summary>
+    ///A test for GetBusDispatcher
+    ///</summary>
+    [TestMethod]
+    public void GetBusDispatcherTest() {
+      Connection conn = _manager.CreateConnection(_hostName, _hostPort);
+      Connection conn2 = _manager.CreateConnection(_hostName, _hostPort);
+      _manager.DefaultConnection = conn;
+      Assert.IsNull(_manager.GetBusDispatcher(conn.BusId));
+      _manager.SetupBusDispatcher(conn2);
+      Assert.AreEqual(_manager.GetBusDispatcher(conn.BusId), conn2);
+      _manager.RemoveBusDispatcher(conn.BusId);
+      Assert.IsNull(_manager.GetBusDispatcher(conn2.BusId));
+      _manager.DefaultConnection = null;
+      Assert.IsNull(_manager.GetBusDispatcher(conn.BusId));
+    }
+
+    /// <summary>
+    ///A test for RemoveBusDispatcher
+    ///</summary>
+    [TestMethod]
+    public void RemoveBusDispatcherTest() {
+      Connection conn = _manager.CreateConnection(_hostName, _hostPort);
+      Connection conn2 = _manager.CreateConnection(_hostName, _hostPort);
+      Connection removed = _manager.RemoveBusDispatcher(conn.BusId);
+      Assert.IsNull(removed);
+      _manager.DefaultConnection = conn;
+      _manager.SetupBusDispatcher(conn2);
+      removed = _manager.RemoveBusDispatcher(conn.BusId);
+      Assert.AreEqual(removed, conn2);
+      _manager.DefaultConnection = null;
+    }
+
+    /// <summary>
+    ///A test for SetupBusDispatcher
+    ///</summary>
+    [TestMethod]
+    public void SetupBusDispatcherTest() {
+      Connection conn = _manager.CreateConnection(_hostName, _hostPort);
+      Connection conn2 = _manager.CreateConnection(_hostName, _hostPort);
+      Assert.IsNull(_manager.GetBusDispatcher(conn.BusId));
+      _manager.DefaultConnection = conn;
+      _manager.ThreadRequester = conn;
+      Assert.IsNull(_manager.GetBusDispatcher(conn.BusId));
+      _manager.SetupBusDispatcher(conn2);
+      Assert.AreEqual(_manager.GetBusDispatcher(conn.BusId), conn2);
+      _manager.RemoveBusDispatcher(conn.BusId);
+      _manager.DefaultConnection = null;
+      _manager.ThreadRequester = null;
+      Assert.IsNull(_manager.GetBusDispatcher(conn.BusId));
+    }
+
+    /// <summary>
+    ///A test for DefaultConnection
+    ///</summary>
+    [TestMethod]
+    public void DefaultConnectionTest() {
+      Connection conn = _manager.CreateConnection(_hostName, _hostPort);
+      Assert.IsNull(_manager.DefaultConnection);
+      _manager.SetupBusDispatcher(conn);
+      _manager.ThreadRequester = conn;
+      Assert.IsNull(_manager.DefaultConnection);
+      _manager.DefaultConnection = conn;
+      Assert.AreEqual(_manager.DefaultConnection, conn);
+      _manager.DefaultConnection = null;
+      _manager.RemoveBusDispatcher(conn.BusId);
+      _manager.ThreadRequester = null;
+    }
+
+    /// <summary>
+    ///A test for ThreadRequester
+    ///</summary>
+    [TestMethod]
+    public void ThreadRequesterTest() {
+      Connection conn = _manager.CreateConnection(_hostName, _hostPort);
+      Assert.IsNull(_manager.ThreadRequester);
+      _manager.SetupBusDispatcher(conn);
+      _manager.DefaultConnection = conn;
+      Assert.IsNull(_manager.ThreadRequester);
+      _manager.ThreadRequester = conn;
+      Assert.AreEqual(_manager.ThreadRequester, conn);
+      _manager.DefaultConnection = null;
+      _manager.RemoveBusDispatcher(conn.BusId);
+      _manager.ThreadRequester = null;
+    }
+  }
+}
---
+++ Test/ConnectionTest.cs
@@ -0,0 +1,226 @@
+﻿using tecgraf.openbus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using tecgraf.openbus.core.v2_00.services.access_control;
+using tecgraf.openbus.core.v2_00.services.offer_registry;
+
+namespace Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ConnectionTest and is intended
+    ///to contain all ConnectionTest Unit Tests
+    ///</summary>
+  [TestClass()]
+  public class ConnectionTest {
+
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext {
+      get {
+        return testContextInstance;
+      }
+      set {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+
+    internal virtual Connection CreateConnection() {
+      // TODO: Instantiate an appropriate concrete class.
+      Connection target = null;
+      return target;
+    }
+
+    /// <summary>
+    ///A test for ExitChain
+    ///</summary>
+    [TestMethod()]
+    public void ExitChainTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      target.ExitChain();
+      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+    }
+
+    /// <summary>
+    ///A test for JoinChain
+    ///</summary>
+    [TestMethod()]
+    public void JoinChainTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      CallerChain chain = null; // TODO: Initialize to an appropriate value
+      target.JoinChain(chain);
+      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+    }
+
+    /// <summary>
+    ///A test for LoginByCertificate
+    ///</summary>
+    [TestMethod()]
+    public void LoginByCertificateTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      string entity = string.Empty; // TODO: Initialize to an appropriate value
+      byte[] privKey = null; // TODO: Initialize to an appropriate value
+      target.LoginByCertificate(entity, privKey);
+      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+    }
+
+    /// <summary>
+    ///A test for LoginByPassword
+    ///</summary>
+    [TestMethod()]
+    public void LoginByPasswordTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      string entity = string.Empty; // TODO: Initialize to an appropriate value
+      byte[] password = null; // TODO: Initialize to an appropriate value
+      target.LoginByPassword(entity, password);
+      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+    }
+
+    /// <summary>
+    ///A test for LoginBySingleSignOn
+    ///</summary>
+    [TestMethod()]
+    public void LoginBySingleSignOnTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      LoginProcess login = null; // TODO: Initialize to an appropriate value
+      byte[] secret = null; // TODO: Initialize to an appropriate value
+      target.LoginBySingleSignOn(login, secret);
+      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+    }
+
+    /// <summary>
+    ///A test for Logout
+    ///</summary>
+    [TestMethod()]
+    public void LogoutTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      bool expected = false; // TODO: Initialize to an appropriate value
+      bool actual;
+      actual = target.Logout();
+      Assert.AreEqual(expected, actual);
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for StartSingleSignOn
+    ///</summary>
+    [TestMethod()]
+    public void StartSingleSignOnTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      byte[] secret = null; // TODO: Initialize to an appropriate value
+      byte[] secretExpected = null; // TODO: Initialize to an appropriate value
+      LoginProcess expected = null; // TODO: Initialize to an appropriate value
+      LoginProcess actual;
+      actual = target.StartSingleSignOn(out secret);
+      Assert.AreEqual(secretExpected, secret);
+      Assert.AreEqual(expected, actual);
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for BusId
+    ///</summary>
+    [TestMethod()]
+    public void BusIdTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      string actual;
+      actual = target.BusId;
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for CallerChain
+    ///</summary>
+    [TestMethod()]
+    public void CallerChainTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      CallerChain actual;
+      actual = target.CallerChain;
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for JoinedChain
+    ///</summary>
+    [TestMethod()]
+    public void JoinedChainTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      CallerChain actual;
+      actual = target.JoinedChain;
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for Login
+    ///</summary>
+    [TestMethod()]
+    public void LoginTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      Nullable<LoginInfo> actual;
+      actual = target.Login;
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for OfferRegistry
+    ///</summary>
+    [TestMethod()]
+    public void OfferRegistryTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      OfferRegistry actual;
+      actual = target.OfferRegistry;
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+
+    /// <summary>
+    ///A test for OnInvalidLoginCallback
+    ///</summary>
+    [TestMethod()]
+    public void OnInvalidLoginCallbackTest() {
+      Connection target = CreateConnection(); // TODO: Initialize to an appropriate value
+      InvalidLoginCallback expected = null; // TODO: Initialize to an appropriate value
+      InvalidLoginCallback actual;
+      target.OnInvalidLoginCallback = expected;
+      actual = target.OnInvalidLoginCallback;
+      Assert.AreEqual(expected, actual);
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+  }
+}
