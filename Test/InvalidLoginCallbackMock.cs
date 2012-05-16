f4a5525011c5bdf033a209032ee45357c1e4b217
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-05-16 21:34:47 +0000

    [OPENBUS-1706] [OPENBUS-1541] Implementar testes para o SDK CSharp 2.0 - Adição da classe mock para o InvalidLoginCallback.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@129105 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ Test/InvalidLoginCallbackMock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tecgraf.openbus.Test {
+  class InvalidLoginCallbackMock : InvalidLoginCallback {
+    public bool InvalidLogin(Connection conn) {
+      throw new NotImplementedException();
+    }
+  }
+}
