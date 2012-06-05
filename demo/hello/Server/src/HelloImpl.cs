71601f1012f62a562db4c435a4dd26e4510e73c4
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-06-05 21:19:04 +0000

    [OPENBUS-1734] [OPENBUS-1541] (CSHARP) Implementar demos para o SDK 2.0 - Correção para que pegue a última entidade da cadeia ao invés da primeira.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@129676 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello/Server/src/HelloImpl.cs
+++ demo/hello/Server/src/HelloImpl.cs
@@ -3,13 +3,11 @@ using tecgraf.openbus;
 using tecgraf.openbus.core.v2_00.services.access_control;
 using tecgraf.openbus.exceptions;
 
-namespace hello
-{
+namespace hello {
   /// <summary>
   /// Implementação do servant Hello.
   /// </summary>  
-  public class HelloImpl : MarshalByRefObject, Hello
-  {
+  public class HelloImpl : MarshalByRefObject, Hello {
     #region Fields
 
     private readonly Connection _conn;
@@ -30,18 +28,22 @@ namespace hello
       try {
         CallerChain chain = _conn.CallerChain;
         if (chain == null) {
-          Console.WriteLine("A cadeia de chamadas é nula, talvez o serviço não esteja logado no barramento. Impossível descobrir quem fez a chamada.");
+          Console.WriteLine(
+            "A cadeia de chamadas é nula, talvez o serviço não esteja logado no barramento. Impossível descobrir quem fez a chamada.");
           Console.WriteLine("Hello World!");
           return;
         }
         LoginInfo[] callers = chain.Callers;
-        Console.WriteLine(String.Format("Hello {0}!", callers[0].entity));
+        Console.WriteLine(String.Format("Hello {0}!",
+                                        callers[callers.Length - 1].entity));
       }
       catch (OpenBusException e) {
-        Console.WriteLine("Erro no método sayHello ao obter a cadeia de chamadas:");
+        Console.WriteLine(
+          "Erro no método sayHello ao obter a cadeia de chamadas:");
         Console.WriteLine(e);
       }
     }
+
     #endregion
   }
 }
