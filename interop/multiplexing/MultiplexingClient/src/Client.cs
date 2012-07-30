2f5a132082caad465d82410b38dc969abf0776ce
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-07-30 23:56:07 +0000

    [OPENBUS-1926]: Alterações nos interops - alterações na demo multiplexing     - remoção de try/catch desnecessário     - propriedades de oferta não precisam ser criadas várias vezes     - remoção de uso de string literal para repid
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@131644 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/multiplexing/MultiplexingClient/src/Client.cs
+++ interop/multiplexing/MultiplexingClient/src/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ch.Elca.Iiop.Idl;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using omg.org.CORBA;
 using tecgraf.openbus.core.v2_0.services.offer_registry;
@@ -11,65 +12,61 @@ namespace tecgraf.openbus.interop.multiplexing {
   [TestClass]
   internal static class Client {
     private static void Main() {
-      try {
-        string hostName = DemoConfig.Default.busHostName;
-        ushort hostPort = DemoConfig.Default.busHostPort;
-        ushort hostPort2 = DemoConfig.Default.bus2HostPort;
-        ASCIIEncoding encoding = new ASCIIEncoding();
-        ushort[] ports = {hostPort, hostPort2};
+      string hostName = DemoConfig.Default.busHostName;
+      ushort hostPort = DemoConfig.Default.busHostPort;
+      ushort hostPort2 = DemoConfig.Default.bus2HostPort;
+      ASCIIEncoding encoding = new ASCIIEncoding();
+      ushort[] ports = {hostPort, hostPort2};
 
-        IDictionary<string, string> props = new Dictionary<string, string>();
-        ConnectionManager manager = ORBInitializer.Manager;
+      IDictionary<string, string> props = new Dictionary<string, string>();
+      ConnectionManager manager = ORBInitializer.Manager;
 
-        foreach (ushort port in ports) {
-          Connection conn = manager.CreateConnection(hostName, port, props);
-          manager.DefaultConnection = conn;
-          const string login = "interop_multiplexing_csharp_client";
-          conn.LoginByPassword(login, encoding.GetBytes(login));
+      foreach (ushort port in ports) {
+        Connection conn = manager.CreateConnection(hostName, port, props);
+        manager.DefaultConnection = conn;
+        const string login = "interop_multiplexing_csharp_client";
+        conn.LoginByPassword(login, encoding.GetBytes(login));
 
-          ServiceProperty[] serviceProperties = new ServiceProperty[2];
-          serviceProperties[0] =
-            new ServiceProperty("openbus.component.interface",
-                                "IDL:tecgraf/openbus/interop/simple/Hello:1.0");
-          serviceProperties[1] = new ServiceProperty("offer.domain",
-                                                     "Interoperability Tests");
-          ServiceOfferDesc[] services =
-            conn.Offers.findServices(serviceProperties);
-          foreach (ServiceOfferDesc offer in services) {
-            foreach (ServiceProperty prop in offer.properties) {
-              if (prop.name.Equals("openbus.offer.entity")) {
-                Console.WriteLine("found offer from " + prop.value +
-                                  " on bus at port " + port);
-              }
-            }
-            try {
-              MarshalByRefObject obj =
-                offer.service_ref.getFacet(
-                  "IDL:tecgraf/openbus/interop/simple/Hello:1.0");
-              if (obj == null) {
-                Console.WriteLine(
-                  "Não foi possível encontrar uma faceta com esse nome.");
-                continue;
-              }
-              Hello hello = obj as Hello;
-              if (hello == null) {
-                Console.WriteLine("Faceta encontrada não implementa Hello.");
-                continue;
-              }
-              string expected = String.Format("Hello {0}@{1}!", login, conn.BusId);
-              string ret = hello.sayHello();
-              Assert.AreEqual(expected, ret);
-            }
-            catch (TRANSIENT) {
-              Console.WriteLine(
-                "Uma das ofertas obtidas é de um cliente inativo. Tentando a próxima.");
+        ServiceProperty[] serviceProperties = new ServiceProperty[2];
+        serviceProperties[0] =
+          new ServiceProperty("openbus.component.interface",
+                              Repository.GetRepositoryID(typeof (Hello)));
+        serviceProperties[1] = new ServiceProperty("offer.domain",
+                                                   "Interoperability Tests");
+        ServiceOfferDesc[] services =
+          conn.Offers.findServices(serviceProperties);
+        foreach (ServiceOfferDesc offer in services) {
+          foreach (ServiceProperty prop in offer.properties) {
+            if (prop.name.Equals("openbus.offer.entity")) {
+              Console.WriteLine("found offer from " + prop.value +
+                                " on bus at port " + port);
             }
           }
-          conn.Logout();
+          try {
+            MarshalByRefObject obj =
+              offer.service_ref.getFacet(
+                Repository.GetRepositoryID(typeof (Hello)));
+            if (obj == null) {
+              Console.WriteLine(
+                "Não foi possível encontrar uma faceta com esse nome.");
+              continue;
+            }
+            Hello hello = obj as Hello;
+            if (hello == null) {
+              Console.WriteLine("Faceta encontrada não implementa Hello.");
+              continue;
+            }
+            string expected = String.Format("Hello {0}@{1}!", login,
+                                            conn.BusId);
+            string ret = hello.sayHello();
+            Assert.AreEqual(expected, ret);
+          }
+          catch (TRANSIENT) {
+            Console.WriteLine(
+              "Uma das ofertas obtidas é de um cliente inativo. Tentando a próxima.");
+          }
         }
-      }
-      catch (Exception e) {
-        Console.WriteLine(e);
+        conn.Logout();
       }
     }
   }
--- interop/multiplexing/MultiplexingServer/src/Server.cs
+++ interop/multiplexing/MultiplexingServer/src/Server.cs
@@ -13,92 +13,88 @@ namespace tecgraf.openbus.interop.multiplexing {
   internal static class Server {
     private static readonly IList<Connection> Conns = new List<Connection>();
 
+    private static readonly ServiceProperty[] ServiceProperties =
+      new[] {new ServiceProperty("offer.domain", "Interoperability Tests")};
+
     private static void Main() {
-      try {
-        string hostName = DemoConfig.Default.busHostName;
-        ushort hostPort = DemoConfig.Default.busHostPort;
-        ushort hostPort2 = DemoConfig.Default.bus2HostPort;
-        const string entity = "interop_multiplexing_csharp_server";
-        Byte[] key = File.ReadAllBytes(DemoConfig.Default.privateKey);
+      string hostName = DemoConfig.Default.busHostName;
+      ushort hostPort = DemoConfig.Default.busHostPort;
+      ushort hostPort2 = DemoConfig.Default.bus2HostPort;
+      const string entity = "interop_multiplexing_csharp_server";
+      Byte[] key = File.ReadAllBytes(DemoConfig.Default.privateKey);
 
-        // setup and start the orb
-        ConnectionManager manager = ORBInitializer.Manager;
+      // setup and start the orb
+      ConnectionManager manager = ORBInitializer.Manager;
 
-        // connect to the bus
-        IDictionary<string, string> props = new Dictionary<string, string>();
-        Connection conn1AtBus1 = manager.CreateConnection(hostName, hostPort, props);
-        Connection conn2AtBus1 = manager.CreateConnection(hostName, hostPort, props);
-        Connection conn3AtBus1 = manager.CreateConnection(hostName, hostPort, props);
-        Connection connAtBus2 = manager.CreateConnection(hostName, hostPort2, props);
+      // connect to the bus
+      IDictionary<string, string> props = new Dictionary<string, string>();
+      Connection conn1AtBus1 = manager.CreateConnection(hostName, hostPort,
+                                                        props);
+      Connection conn2AtBus1 = manager.CreateConnection(hostName, hostPort,
+                                                        props);
+      Connection conn3AtBus1 = manager.CreateConnection(hostName, hostPort,
+                                                        props);
+      Connection connAtBus2 = manager.CreateConnection(hostName, hostPort2,
+                                                       props);
 
-        Conns.Add(conn1AtBus1);
-        Conns.Add(conn2AtBus1);
-        Conns.Add(conn3AtBus1);
-        Conns.Add(connAtBus2);
+      Conns.Add(conn1AtBus1);
+      Conns.Add(conn2AtBus1);
+      Conns.Add(conn3AtBus1);
+      Conns.Add(connAtBus2);
 
-        // create service SCS component
-        ComponentId id = new ComponentId("Hello", 1, 0, 0, ".net");
-        ComponentContext component = new DefaultComponentContext(id);
-        component.AddFacet("Hello", Repository.GetRepositoryID(typeof (Hello)),
-                           new HelloImpl(Conns));
-        IComponent ic = component.GetIComponent();
+      // create service SCS component
+      ComponentId id = new ComponentId("Hello", 1, 0, 0, ".net");
+      ComponentContext component = new DefaultComponentContext(id);
+      component.AddFacet("Hello", Repository.GetRepositoryID(typeof (Hello)),
+                         new HelloImpl(Conns));
+      IComponent ic = component.GetIComponent();
 
-        // login to the bus
-        conn1AtBus1.LoginByCertificate(entity, key);
-        conn2AtBus1.LoginByCertificate(entity, key);
-        conn3AtBus1.LoginByCertificate(entity, key);
-        connAtBus2.LoginByCertificate(entity, key);
+      // login to the bus
+      conn1AtBus1.LoginByCertificate(entity, key);
+      conn2AtBus1.LoginByCertificate(entity, key);
+      conn3AtBus1.LoginByCertificate(entity, key);
+      connAtBus2.LoginByCertificate(entity, key);
 
-        // set incoming connections
-        manager.SetDispatcher(conn1AtBus1);
-        manager.SetDispatcher(connAtBus2);
+      // set incoming connections
+      manager.SetDispatcher(conn1AtBus1);
+      manager.SetDispatcher(connAtBus2);
 
-        RegisterThreadStart start1 = new RegisterThreadStart(conn1AtBus1,
-                                                             component.
-                                                               GetIComponent());
-        Thread thread1 = new Thread(start1.Run);
-        thread1.Start();
-        conn1AtBus1.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
-                                                                   ic,
-                                                                   GetProps());
+      RegisterThreadStart start1 = new RegisterThreadStart(conn1AtBus1,
+                                                           component.
+                                                             GetIComponent());
+      Thread thread1 = new Thread(start1.Run);
+      thread1.Start();
+      conn1AtBus1.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
+                                                                 ic,
+                                                                 ServiceProperties);
 
-        RegisterThreadStart start2 = new RegisterThreadStart(conn2AtBus1,
-                                                             component.
-                                                               GetIComponent());
-        Thread thread2 = new Thread(start2.Run);
-        thread2.Start();
-        conn2AtBus1.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
-                                                                   ic,
-                                                                   GetProps());
+      RegisterThreadStart start2 = new RegisterThreadStart(conn2AtBus1,
+                                                           component.
+                                                             GetIComponent());
+      Thread thread2 = new Thread(start2.Run);
+      thread2.Start();
+      conn2AtBus1.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
+                                                                 ic,
+                                                                 ServiceProperties);
 
-        RegisterThreadStart start3 = new RegisterThreadStart(conn3AtBus1,
-                                                             component.
-                                                               GetIComponent());
-        Thread thread3 = new Thread(start3.Run);
-        thread3.Start();
-        conn3AtBus1.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
-                                                                   ic,
-                                                                   GetProps());
+      RegisterThreadStart start3 = new RegisterThreadStart(conn3AtBus1,
+                                                           component.
+                                                             GetIComponent());
+      Thread thread3 = new Thread(start3.Run);
+      thread3.Start();
+      conn3AtBus1.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
+                                                                 ic,
+                                                                 ServiceProperties);
 
-        manager.Requester = connAtBus2;
-        connAtBus2.Offers.registerService(ic, GetProps());
-        connAtBus2.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
-                                                                  ic, GetProps());
+      manager.Requester = connAtBus2;
+      connAtBus2.Offers.registerService(ic, ServiceProperties);
+      connAtBus2.OnInvalidLogin = new HelloInvalidLoginCallback(entity, key,
+                                                                ic,
+                                                                ServiceProperties);
 
-        Console.WriteLine("Servidor no ar.");
+      Console.WriteLine("Servidor no ar.");
 
-        Thread.Sleep(Timeout.Infinite);
-      }
-      catch (Exception e) {
-        Console.WriteLine(e);
-      }
-    }
-
-    private static ServiceProperty[] GetProps() {
-      ServiceProperty[] serviceProperties = new ServiceProperty[1];
-      serviceProperties[0] = new ServiceProperty("offer.domain",
-                                                 "Interoperability Tests");
-      return serviceProperties;
+      Thread.Sleep(Timeout.Infinite);
     }
 
     private class RegisterThreadStart {
@@ -112,12 +108,7 @@ namespace tecgraf.openbus.interop.multiplexing {
 
       public void Run() {
         ORBInitializer.Manager.Requester = _conn;
-        try {
-          _conn.Offers.registerService(_component, GetProps());
-        }
-        catch (Exception e) {
-          Console.WriteLine(e);
-        }
+        _conn.Offers.registerService(_component, ServiceProperties);
       }
     }
   }
