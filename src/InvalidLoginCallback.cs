747ade9d010288a1fc66e19977bffa33dd029159
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-08-06 18:15:45 +0000

    [OPENBUS-1901]: (CSharp) Documentar API da biblioteca de acesso do OpenBus 2.0 - incluindo informação de que caso ocorra uma exceção na execução da calback o erro será apenas logado.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@131939 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/InvalidLoginCallback.cs
+++ src/InvalidLoginCallback.cs
@@ -12,7 +12,9 @@ namespace tecgraf.openbus {
 
     /// <summary>
     /// Método que será chamado quando uma notificação de login inválido for
-    /// recebida.
+    /// recebida. Caso alguma exceção ocorra durante a execução do método e não
+    /// seja tratada, o erro será capturado pelo interceptador e registrado no
+    /// log.
     /// </summary>
     /// <param name="conn">Conexão que recebeu a notificação de login inválido.</param>
     /// <param name="login">Informações do login que se tornou inválido.</param>
