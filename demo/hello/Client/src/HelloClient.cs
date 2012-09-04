fd7c576307e6d8e9287920287cb56ee3aba7c9f7
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-09-04 21:51:49 +0000

    [OPENBUS-2021]: Adequar demos do SDK às modificações definidas pelo Maia - bugfix: caso a senha não seja fornecida, o login do cliente deve ser usado como senha (estava usando o do servidor).
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@133081 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello/Client/src/HelloClient.cs
+++ demo/hello/Client/src/HelloClient.cs
@@ -19,7 +19,7 @@ namespace demo {
       ushort port = Convert.ToUInt16(args[1]);
       string helloEntity = args[2];
       string entity = args[3];
-      byte[] password = new ASCIIEncoding().GetBytes(args[4] ?? helloEntity);
+      byte[] password = new ASCIIEncoding().GetBytes(args[4] ?? entity);
 
       // Cria conexão e a define como conexão padrão tanto para entrada como saída.
       // O uso exclusivo da conexão padrão (sem uso de current e callback de despacho) só é recomendado para aplicações que criem apenas uma conexão e desejem utilizá-la em todos os casos. Para situações diferentes, consulte o manual do SDK OpenBus e/ou outras demos.
