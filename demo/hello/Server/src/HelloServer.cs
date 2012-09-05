64d7cc7ae41e87a948218eb968009a9621ba9333
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2012-09-05 18:40:37 +0000

    [OPENBUS-2021]: Adequar demos do SDK às modificações definidas pelo Maia - Correção de comentário
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/openbus_v2_proto@133120 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello/Server/src/HelloServer.cs
+++ demo/hello/Server/src/HelloServer.cs
@@ -45,7 +45,7 @@ namespace demo {
                                            };
 
       // Cria conexão e a define como conexão padrão tanto para entrada como saída.
-      // O uso exclusivo da conexão padrão (sem uso de requester e dispatcher) só é recomendado para aplicações que criem apenas uma conexão e desejem utilizá-la em todos os casos. Para situações diferentes, consulte o manual do SDK OpenBus e/ou outras demos.
+      // O uso exclusivo da conexão padrão (sem uso de current e callback de despacho) só é recomendado para aplicações que criem apenas uma conexão e desejem utilizá-la em todos os casos. Para situações diferentes, consulte o manual do SDK OpenBus e/ou outras demos.
       OpenBusContext context = ORBInitializer.Context;
       _conn = context.CreateConnection(host, port, null);
       context.SetDefaultConnection(_conn);
