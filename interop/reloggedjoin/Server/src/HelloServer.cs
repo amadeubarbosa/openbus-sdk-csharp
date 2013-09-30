0c183b98cb3e3e841c25e998e3298ba6c938427f
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2013-09-30 18:53:21 +0000

    [OPENBUS-2303 / OPENBUS-2327] (CSharp) Permitir fazer chamadas dentro de cadeias (join) recebidas com um login diferente, mas da mesma entidade - Finalização da demo. - Commit final de implementação.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/02_00_00@145041 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/reloggedjoin/Server/src/HelloServer.cs
+++ interop/reloggedjoin/Server/src/HelloServer.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Threading;
 using Ch.Elca.Iiop.Idl;
 using Scs.Core;
+using log4net.Config;
 using scs.core;
 using tecgraf.openbus.core.v2_0.services.access_control;
 using tecgraf.openbus.core.v2_0.services.offer_registry;
@@ -10,12 +12,17 @@ using tecgraf.openbus.interop.relloggedjoin.Properties;
 using tecgraf.openbus.interop.simple;
 using tecgraf.openbus.security;
 
-namespace tecgraf.openbus.interop.relloggedjoin.src {
-  internal static class Proxy {
-    private const string Entity = "interop_reloggedjoin_csharp_proxy";
+namespace tecgraf.openbus.interop.relloggedjoin {
+  /// <summary>
+  ///   Servidor do teste de interoperabilidade reloggedjoin.
+  /// </summary>
+  internal static class HelloServer {
+    private const string Entity = "interop_reloggedjoin_csharp_server";
+    private static Connection _conn;
     private static PrivateKey _privateKey;
     private static IComponent _ic;
     private static ServiceProperty[] _properties;
+    private static ServiceOffer _offer;
 
     private static void Main() {
       AppDomain.CurrentDomain.ProcessExit += CurrentDomainProcessExit;
@@ -23,42 +30,57 @@ namespace tecgraf.openbus.interop.relloggedjoin.src {
       ushort hostPort = DemoConfig.Default.busHostPort;
       _privateKey = Crypto.ReadKeyFile(DemoConfig.Default.privateKey);
 
+      FileInfo logFileInfo = new FileInfo(DemoConfig.Default.openbusLogFile);
+      XmlConfigurator.ConfigureAndWatch(logFileInfo);
+
       ConnectionProperties props = new ConnectionPropertiesImpl();
       props.AccessKey = _privateKey;
       OpenBusContext context = ORBInitializer.Context;
-      Connection conn = context.CreateConnection(hostName, hostPort, props);
-      context.SetDefaultConnection(conn);
-
-      conn.LoginByCertificate(Entity, _privateKey);
+      _conn = context.CreateConnection(hostName, hostPort, props);
+      context.SetDefaultConnection(_conn);
 
       ComponentContext component =
-        new DefaultComponentContext(new ComponentId("Hello", 1, 0, 0, ".net"));
-      HelloProxyImpl hello = new HelloProxyImpl(Entity, _privateKey);
-      component.AddFacet("Hello",
-                         Repository.GetRepositoryID(typeof (Hello)),
-                         hello);
+        new DefaultComponentContext(new ComponentId("hello", 1, 0, 0, ".net"));
+      component.AddFacet("Hello", Repository.GetRepositoryID(typeof (Hello)),
+                         new HelloImpl());
+
+      _conn.LoginByCertificate(Entity, _privateKey);
 
       _ic = component.GetIComponent();
       _properties = new[] {
         new ServiceProperty("offer.domain",
                             "Interoperability Tests"),
         new ServiceProperty("reloggedjoin.role",
-                            "proxy")
+                            "server")
       };
-      context.OfferRegistry.registerService(_ic, _properties);
-      conn.OnInvalidLogin = InvalidLogin;
+      _offer = context.OfferRegistry.registerService(_ic, _properties);
+      _conn.OnInvalidLogin = InvalidLogin;
 
-      Console.WriteLine("Hello Proxy no ar.");
-
+      Console.WriteLine("Servidor no ar.");
       Thread.Sleep(Timeout.Infinite);
     }
 
+    private static void CurrentDomainProcessExit(object sender, EventArgs e) {
+      if (_offer != null) {
+        try {
+          _offer.remove();
+        }
+        catch (Exception exc) {
+          Console.WriteLine(
+            "Erro ao remover a oferta antes de finalizar o processo: " + exc);
+        }
+      }
+      _conn.Logout();
+    }
+
     private static void InvalidLogin(Connection conn, LoginInfo login) {
+      OpenBusContext context = ORBInitializer.Context;
+      context.SetCurrentConnection(conn);
       try {
         Console.WriteLine(
           "Callback de InvalidLogin foi chamada, tentando logar novamente no barramento.");
         conn.LoginByCertificate(Entity, _privateKey);
-        ORBInitializer.Context.OfferRegistry.registerService(_ic, _properties);
+        _offer = context.OfferRegistry.registerService(_ic, _properties);
       }
       catch (AlreadyLoggedInException) {
         // outra thread reconectou
@@ -66,10 +88,6 @@ namespace tecgraf.openbus.interop.relloggedjoin.src {
       catch (Exception e) {
         Console.WriteLine(e);
       }
-    }
-
-    private static void CurrentDomainProcessExit(object sender, EventArgs e) {
-      ORBInitializer.Context.GetCurrentConnection().Logout();
     }
   }
 }
