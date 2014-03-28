8512d54b180b8c90a75fd7fbc1b000eb44465a04
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2014-03-28 18:03:00 +0000

    [OPENBUS-2435] Inclusão de suporte a serialização de cadeias em formato interoperável. [OPENBUS-2443][C#] Inclusão de suporte a serialização de cadeias em formato interoperável. [OPENBUS-2434] Inclusão de operação na biblioteca de acesso para criar a cadeia resultante de uma chamada para um dado login válido. [OPENBUS-2438][C#] Inclusão de operação na biblioteca de acesso para criar a cadeia resultante de uma chamada para um dado login válido. - Faltou incluir um arquivo relativo aos testes.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/trunk@151015 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ Test/CallerChainInspectorImpl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using tecgraf.openbus.core.v2_0.services.access_control;
+using test;
+
+namespace tecgraf.openbus.test {
+  public class CallerChainInspectorImpl : MarshalByRefObject, CallerChainInspector {
+  /**
+   * O Contexto.
+   */
+  private readonly OpenBusContext _context;
+
+  /**
+   * Construtor.
+   * 
+   * @param context o contexto.
+   */
+  public CallerChainInspectorImpl() {
+    _context = ORBInitializer.Context;
+  }
+
+  public string[] listCallers() {
+    CallerChain chain = _context.CallerChain;
+    IList<String> list = new List<String>();
+    foreach (LoginInfo info in chain.Originators) {
+      list.Add(info.entity);
+    }
+    list.Add(chain.Caller.entity);
+    string[] array = new string[list.Count];
+    list.CopyTo(array, 0);
+    return array;
+  }
+
+  public string[] listCallerLogins() {
+    CallerChain chain = _context.CallerChain;
+    IList<String> list = new List<String>();
+    foreach (LoginInfo info in chain.Originators) {
+      list.Add(info.id);
+    }
+    list.Add(chain.Caller.id);
+    string[] array = new string[list.Count];
+    list.CopyTo(array, 0);
+    return array;
+  }
+
+  }
+}
