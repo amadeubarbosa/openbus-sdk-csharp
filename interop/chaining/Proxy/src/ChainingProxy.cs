6247f56219cc199faca5da86eb33981b79d16196
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2014-10-27 17:36:04 +0000

    Merged revision(s) 157341-157573 from openbus/sdk/csharp/trunk/interop: [OPENBUS-2552] [C#] Implementar teste de interoperabilidade da serialização de cadeias - Inclusão do interop chaining.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/branches/02_00_01@157583 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ interop/chaining/Proxy/src/ChainingProxy.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Ch.Elca.Iiop.Idl;
+using Scs.Core;
+using log4net.Config;
+using scs.core;
+using tecgraf.openbus.core.v2_1.services.access_control;
+using tecgraf.openbus.core.v2_1.services.offer_registry;
+using tecgraf.openbus.exceptions;
+using tecgraf.openbus.interop.chaining.Properties;
+using tecgraf.openbus.security;
+
+namespace tecgraf.openbus.interop.chaining {
+  /// <summary>
+  /// Proxy do teste de interoperabilidade chaining.
+  /// </summary>
+  internal static class ChainingProxy {
+    private const string Entity = "interop_chaining_csharp_proxy";
+    private static Connection _conn;
+    private static PrivateKey _privateKey;
+    private static IComponent _ic;
+    private static ServiceProperty[] _properties;
+    private static ServiceOffer _offer;
+
+    private static void Main() {
+      AppDomain.CurrentDomain.ProcessExit += CurrentDomainProcessExit;
+      string hostName = DemoConfig.Default.busHostName;
+      ushort hostPort = DemoConfig.Default.busHostPort;
+      _privateKey = Crypto.ReadKeyFile(DemoConfig.Default.privateKey);
+
+      FileInfo logFileInfo = new FileInfo(DemoConfig.Default.openbusLogFile);
+      XmlConfigurator.ConfigureAndWatch(logFileInfo);
+
+      ConnectionProperties props = new ConnectionPropertiesImpl();
+      props.AccessKey = _privateKey;
+      ORBInitializer.InitORB();
+      OpenBusContext context = ORBInitializer.Context;
+      _conn = context.CreateConnection(hostName, hostPort, props);
+      context.SetDefaultConnection(_conn);
+
+      ComponentContext component =
+        new DefaultComponentContext(new ComponentId("HelloProxy", 1, 0, 0, ".net"));
+      component.AddFacet("HelloProxy", Repository.GetRepositoryID(typeof(HelloProxy)),
+                         new ProxyImpl());
+
+      _conn.LoginByCertificate(Entity, _privateKey);
+
+      _ic = component.GetIComponent();
+      _properties = new[] {
+                            new ServiceProperty("offer.domain",
+                                                "Interoperability Tests")
+                          };
+      _offer = context.OfferRegistry.registerService(_ic, _properties);
+      _conn.OnInvalidLogin = InvalidLogin;
+
+      Console.WriteLine("Servidor no ar.");
+      Thread.Sleep(Timeout.Infinite);
+    }
+
+    private static void CurrentDomainProcessExit(object sender, EventArgs e) {
+      if (_offer != null) {
+        try {
+          _offer.remove();
+        }
+        catch (Exception exc) {
+          Console.WriteLine(
+            "Erro ao remover a oferta antes de finalizar o processo: " + exc);
+        }
+      }
+      _conn.Logout();
+    }
+
+    private static void InvalidLogin(Connection conn, LoginInfo login) {
+      OpenBusContext context = ORBInitializer.Context;
+      context.SetCurrentConnection(conn);
+      try {
+        Console.WriteLine(
+          "Callback de InvalidLogin foi chamada, tentando logar novamente no barramento.");
+        conn.LoginByCertificate(Entity, _privateKey);
+        _offer = context.OfferRegistry.registerService(_ic, _properties);
+      }
+      catch (AlreadyLoggedInException) {
+        // outra thread reconectou
+      }
+      catch (Exception e) {
+        Console.WriteLine(e);
+      }
+    }
+  }
+}
---
+++ interop/chaining/Server/src/HelloImpl.cs
@@ -0,0 +1,31 @@
+using System;
+using tecgraf.openbus.core.v2_1.services.access_control;
+using tecgraf.openbus.interop.simple;
+
+namespace tecgraf.openbus.interop.chaining {
+  /// <summary>
+  /// Implementação do servant Hello.
+  /// </summary>  
+  public class HelloImpl : MarshalByRefObject, Hello {
+    #region Hello Members
+
+    public string sayHello() {
+      try {
+        LoginInfo caller = ORBInitializer.Context.CallerChain.Originators[0];
+        string hello = String.Format("Hello {0}!", caller.entity);
+        Console.WriteLine(hello);
+        return hello;
+      }
+      catch (Exception e) {
+        Console.WriteLine(e.StackTrace);
+      }
+      return "Hello World!";
+    }
+
+    #endregion
+
+    public override object InitializeLifetimeService() {
+      return null;
+    }
+  }
+}
