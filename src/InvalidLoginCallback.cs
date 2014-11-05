a49d82ca8e7451cc3f5ad3e2e219dfe28521e0ab
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2014-11-05 16:27:17 +0000

    Merged revision(s) 158053 from openbus/sdk/csharp/branches/02_00_01: [OPENBUS-2626] (C#) Documentar sobre possibilidade de lib entrar em loop infinito caso callback de invalid login realize uma chamada remota qualquer antes de tentar refazer o login
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/csharp/trunk@158054 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/InvalidLoginCallback.cs
+++ src/InvalidLoginCallback.cs
@@ -8,6 +8,13 @@ namespace tecgraf.openbus {
   /// recebida. Caso alguma exceção ocorra durante a execução do método e não
   /// seja tratada, o erro será capturado pelo interceptador e registrado no
   /// log.
+  /// 
+  /// É importante observar que a primeira chamada remota que esta callback
+  /// realizar deve ser uma tentativa de autenticação junto ao barramento
+  /// (loginBy*). Caso a callback realize qualquer outra chamada remota a mesma
+  /// potencialmenete ocasionará um laço infinito, pois esta outra chamada irá
+  /// falhar devido ao login invalidado, chamando novamente a callback para
+  /// tentar refazer a autenticação.
   /// </summary>
   /// <param name="conn">Conexão que recebeu a notificação de login inválido.</param>
   /// <param name="login">Informações do login que se tornou inválido.</param>
